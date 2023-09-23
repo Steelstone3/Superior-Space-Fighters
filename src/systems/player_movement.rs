4dfebbfabc91c67b6eb784eec31bfddbfe03f66d
Skippy <steelstone3@gmail.com>
2023-09-23 12:29:49 +0100

    player with speed

--- src/systems/player_movement.rs
+++ src/systems/player_movement.rs
@@ -1,53 +1,54 @@
 use bevy::{
     prelude::{Input, KeyCode, Query, Res, Transform},
-    sprite::Sprite,
     time::Time,
 };
 
+use crate::components::player::Player;
+
 pub fn player_movement(
-    mut characters: Query<(&mut Transform, &Sprite)>,
+    mut characters: Query<(&mut Transform, &Player)>,
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    let player_speed = 400.0;
-    let player_diagonal_speed = calculate_diagonal_speed(player_speed);
+    for (mut transform, player) in &mut characters {
+        let player_speed = player.speed * time.delta_seconds();
+        let player_diagonal_speed = calculate_diagonal_speed(player_speed);
 
-    for (mut transform, _) in &mut characters {
         // Forward right diagonal
         if input.pressed(KeyCode::W) && input.pressed(KeyCode::D) {
-            transform.translation.y += player_diagonal_speed * time.delta_seconds();
-            transform.translation.x += player_diagonal_speed * time.delta_seconds();
+            transform.translation.y += player_diagonal_speed;
+            transform.translation.x += player_diagonal_speed;
         }
         // Forward left diagonal
         else if input.pressed(KeyCode::W) && input.pressed(KeyCode::A) {
-            transform.translation.y += player_diagonal_speed * time.delta_seconds();
-            transform.translation.x -= player_diagonal_speed * time.delta_seconds();
+            transform.translation.y += player_diagonal_speed;
+            transform.translation.x -= player_diagonal_speed;
         }
         // Downward right diagonal
         else if input.pressed(KeyCode::S) && input.pressed(KeyCode::D) {
-            transform.translation.y -= player_diagonal_speed * time.delta_seconds();
-            transform.translation.x += player_diagonal_speed * time.delta_seconds();
+            transform.translation.y -= player_diagonal_speed;
+            transform.translation.x += player_diagonal_speed;
         }
         // Downward left diagonal
         else if input.pressed(KeyCode::S) && input.pressed(KeyCode::A) {
-            transform.translation.y -= player_diagonal_speed * time.delta_seconds();
-            transform.translation.x -= player_diagonal_speed * time.delta_seconds();
+            transform.translation.y -= player_diagonal_speed;
+            transform.translation.x -= player_diagonal_speed;
         }
         // Forward
         else if input.pressed(KeyCode::W) {
-            transform.translation.y += player_speed * time.delta_seconds();
+            transform.translation.y += player_speed;
         }
         // Downward
         else if input.pressed(KeyCode::S) {
-            transform.translation.y -= player_speed * time.delta_seconds();
+            transform.translation.y -= player_speed;
         }
         // Right
         else if input.pressed(KeyCode::D) {
-            transform.translation.x += player_speed * time.delta_seconds();
+            transform.translation.x += player_speed;
         }
         // Left
         else if input.pressed(KeyCode::A) {
-            transform.translation.x -= player_speed * time.delta_seconds();
+            transform.translation.x -= player_speed;
         }
     }
 }
--- src/systems/spawn_sprite.rs
+++ src/systems/spawn_sprite.rs
@@ -3,15 +3,19 @@ use bevy::{
     sprite::{Sprite, SpriteBundle},
 };
 
+use crate::components::player::Player;
+
 pub fn spawn_sprite(mut commands: Commands, _asset_server: Res<AssetServer>) {
     //    let texture = asset_server.load("player_character.png");
 
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(100.0, 100.0)),
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(100.0, 100.0)),
+                ..Default::default()
+            },
+            // texture,
             ..Default::default()
-        },
-        // texture,
-        ..Default::default()
-    });
+        })
+        .insert(Player { speed: 400.0 });
 }
