5599236704be13acf21e80cdeebc62b451e905ce
Skippy <steelstone3@gmail.com>
2023-09-23 12:22:28 +0100

    Update spawn_sprite.rs

--- src/systems/spawn_sprite.rs
+++ src/systems/spawn_sprite.rs
@@ -1,8 +1,11 @@
-use bevy::{prelude::{Commands, Vec2, AssetServer, Res}, sprite::{SpriteBundle, Sprite}};
+use bevy::{
+    prelude::{AssetServer, Commands, Res, Vec2},
+    sprite::{Sprite, SpriteBundle},
+};
 
 pub fn spawn_sprite(mut commands: Commands, _asset_server: Res<AssetServer>) {
-//    let texture = asset_server.load("player_character.png");
-   
+    //    let texture = asset_server.load("player_character.png");
+
     commands.spawn(SpriteBundle {
         sprite: Sprite {
             custom_size: Some(Vec2::new(100.0, 100.0)),
