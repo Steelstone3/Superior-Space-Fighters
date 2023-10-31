fc99f44437d4a3789a2d1955b61e72cca1155bcf
Skippy <steelstone3@gmail.com>
2023-10-31 19:46:23 +0000

    Update player_movement.rs

--- src/systems/player/player_movement.rs
+++ src/systems/player/player_movement.rs
@@ -15,15 +15,15 @@ pub fn player_movement(
 
         // Forward
         if input.pressed(KeyCode::W) {
-            player.current_velocity =
-                (player.current_velocity + player.acceleration).clamp(-player.velocity, player.velocity);
+            player.current_velocity = (player.current_velocity + player.acceleration)
+                .clamp(-player.velocity, player.velocity);
 
             let movement_direction = transform.rotation * Vec3::Y;
             let translation_delta = movement_direction * player_speed;
             transform.translation += translation_delta;
         } else if input.pressed(KeyCode::S) {
-            player.current_velocity =
-                (player.current_velocity - player.acceleration).clamp(-player.velocity, player.velocity);
+            player.current_velocity = (player.current_velocity - player.acceleration)
+                .clamp(-player.velocity, player.velocity);
 
             let movement_direction = transform.rotation * Vec3::Y;
             let translation_delta = movement_direction * player_speed;
@@ -34,16 +34,17 @@ pub fn player_movement(
             let movement_direction = transform.rotation * Vec3::Y;
 
             if player.current_velocity > 0.0 {
-                player.current_velocity =
-                    (player.current_velocity - player.acceleration).clamp(-player.velocity, player.velocity);
+                player.current_velocity = (player.current_velocity - player.acceleration)
+                    .clamp(-player.velocity, player.velocity);
                 let translation_delta = movement_direction * -player_speed;
                 transform.translation -= translation_delta;
             } else if player.current_velocity < 0.0 {
-                player.current_velocity = (player.current_velocity + player.acceleration).clamp(-player.velocity, player.velocity);
+                player.current_velocity = (player.current_velocity + player.acceleration)
+                    .clamp(-player.velocity, player.velocity);
                 let translation_delta = movement_direction * player_speed;
                 transform.translation += translation_delta;
             }
-            
+
             info!("current velocity {:?} ", player.current_velocity);
         }
 
