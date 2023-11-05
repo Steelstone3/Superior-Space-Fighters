6297e18941f790bfd2ef2538fb2028af29e31ae3
Skippy <steelstone3@gmail.com>
2023-11-05 16:10:23 +0000

    update

--- src/systems/stations/spawn_station.rs
+++ src/systems/stations/spawn_station.rs
@@ -1,6 +1,6 @@
 use crate::{
-    components::starship::Starship,
-    systems::controllers::random_generator::{generate_seed, random_range_f32, random_range_i32},
+    components::station::Station,
+    systems::controllers::random_generator::{generate_seed, random_range_f32},
 };
 use bevy::{
     prelude::{AssetServer, Commands, Res, Vec2, Vec3},
@@ -9,21 +9,17 @@ use bevy::{
 use rand::random;
 
 pub fn spawn_random_station(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let ship = Starship {
+    let station = Station {
         asset: random(),
-        velocity: 30.0,
-        rotation: f32::to_radians(10.0),
-        size: Vec2 { x: 100.0, y: 100.0 },
-        acceleration: 0.1,
-        current_velocity: 0.0,
+        size: Vec2 { x: 500.0, y: 500.0 },
     };
 
-    let texture = asset_server.load(ship.asset.to_string());
+    let texture = asset_server.load(station.asset.to_string());
 
     commands
         .spawn(SpriteBundle {
             sprite: Sprite {
-                custom_size: Some(ship.size),
+                custom_size: Some(station.size),
                 ..Default::default()
             },
             texture,
@@ -31,11 +27,11 @@ pub fn spawn_random_station(mut commands: Commands, asset_server: Res<AssetServe
                 translation: Vec3::new(
                     random_range_f32(generate_seed(), -320.0, 320.0),
                     random_range_f32(generate_seed(), -240.0, 240.0),
-                    3.0,
+                    1.0,
                 ),
                 ..Default::default()
             },
             ..Default::default()
         })
-        .insert(ship);
+        .insert(station);
 }
