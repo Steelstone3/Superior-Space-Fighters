8e73ee126a8336d01533c1c78cb8cfc2c5239d5c
Skippy <steelstone3@gmail.com>
2023-11-20 00:07:55 +0000

    update

--- src/systems/camera/increment_camera.rs
+++ src/systems/camera/increment_camera.rs
@@ -1,28 +1,31 @@
+use crate::{
+    components::player_starship::PlayerStarship, resources::camera_settings::CameraSettings,
+};
 use bevy::{
-    prelude::{Input, KeyCode, Query, Res, Transform, Vec3},
+    prelude::{
+        Camera, Input, KeyCode, OrthographicProjection, Query, Res, ResMut, Transform, Vec3, With,
+    },
     time::Time,
 };
 
-use crate::components::player_starship::PlayerStarship;
-
 pub fn increment_camera(
-    mut characters: Query<(&mut Transform, &mut PlayerStarship)>,
     input: Res<Input<KeyCode>>,
-    time: Res<Time>,
+    mut camera_projection_query: Query<&mut OrthographicProjection, With<Camera>>,
+    mut camera_settings: ResMut<CameraSettings>,
 ) {
-    for (mut transform, mut player) in &mut characters {
-        let player_speed = player.ship.current_velocity * time.delta_seconds();
+    // for (mut transform, mut player) in &mut characters {
+    // let player_speed = player.ship.current_velocity * time.delta_seconds();
 
-        // Forwards
-        if input.pressed(KeyCode::W) {
-            player.ship.current_velocity = (player.ship.current_velocity
-                + player.ship.acceleration)
-                .clamp(-player.ship.velocity, player.ship.velocity);
+    // Increment Camera Zoom
+    // if input.pressed(KeyCode::Plus) {
+    //     player.ship.current_velocity = (player.ship.current_velocity
+    //         + player.ship.acceleration)
+    //         .clamp(-player.ship.velocity, player.ship.velocity);
 
-            let movement_direction = transform.rotation * Vec3::Y;
-            let translation_delta = movement_direction * player_speed;
-            transform.translation += translation_delta;
-        }
-        // Backwards
-    }
+    //     let movement_direction = transform.rotation * Vec3::Y;
+    //     let translation_delta = movement_direction * player_speed;
+    //     transform.translation += translation_delta;
+    // }
+    // Backwards
+    // }
 }
