05d257a7890d9d19a231297c23d79d3e6109aab6
Skippy <steelstone3@gmail.com>
2023-11-20 00:17:54 +0000

    key binds

--- src/systems/camera/increment_camera.rs
+++ src/systems/camera/increment_camera.rs
@@ -1,9 +1,21 @@
 use crate::resources::camera_settings::CameraSettings;
-use bevy::prelude::{Camera, Input, KeyCode, OrthographicProjection, Query, Res, ResMut, With};
+use bevy::{
+    prelude::{Camera, Input, KeyCode, OrthographicProjection, Query, Res, ResMut, With},
+    utils::tracing,
+};
 
 pub fn increment_camera(
-    _input: Res<Input<KeyCode>>,
+    input: Res<Input<KeyCode>>,
     _camera_projection_query: Query<&mut OrthographicProjection, With<Camera>>,
     _camera_settings: ResMut<CameraSettings>,
 ) {
+    if input.just_pressed(KeyCode::Equals) {
+        tracing::info!("Camera zoomed in");
+    }
+    if input.just_pressed(KeyCode::Minus) {
+        tracing::info!("Camera zoomed out");
+    }
+    if input.just_pressed(KeyCode::Key0) {
+        tracing::info!("Camera zoom reset")
+    }
 }
