1bf9203ca80c57174cf712ae2a13b1dac27e61d2
Skippy <steelstone3@gmail.com>
2023-11-20 23:03:20 +0000

    Update increment_camera.rs

--- src/systems/camera/increment_camera.rs
+++ src/systems/camera/increment_camera.rs
@@ -3,19 +3,31 @@ use bevy::{
     prelude::{Camera, Input, KeyCode, OrthographicProjection, Query, Res, ResMut, With},
     utils::tracing,
 };
+use float_lerp::lerp;
 
 pub fn increment_camera(
     input: Res<Input<KeyCode>>,
-    _camera_projection_query: Query<&mut OrthographicProjection, With<Camera>>,
-    _camera_settings: ResMut<CameraSettings>,
+    mut camera_projection_query: Query<&mut OrthographicProjection, With<Camera>>,
+    mut camera_settings: ResMut<CameraSettings>,
 ) {
+    let mut camera_projection = camera_projection_query.single_mut();
+
     if input.just_pressed(KeyCode::Equals) {
+        camera_settings.current_zoom = (camera_settings.current_zoom * 0.75
+            / camera_settings.zoom_speed)
+            .clamp(camera_settings.min_zoom, camera_settings.max_zoom);
         tracing::info!("Camera zoomed in");
     }
     if input.just_pressed(KeyCode::Minus) {
+        camera_settings.current_zoom =
+            (camera_settings.current_zoom * 1.25 * camera_settings.zoom_speed)
+                .clamp(camera_settings.min_zoom, camera_settings.max_zoom);
         tracing::info!("Camera zoomed out");
     }
     if input.just_pressed(KeyCode::Key0) {
+        camera_settings.current_zoom = 1.0;
         tracing::info!("Camera zoom reset")
     }
+
+    camera_projection.scale = lerp(camera_projection.scale, camera_settings.current_zoom, 0.1);
 }
