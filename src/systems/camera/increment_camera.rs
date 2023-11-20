9993acd80db779d92631e6c119bd8b7a7de2c4be
Skippy <steelstone3@gmail.com>
2023-11-20 00:11:09 +0000

    update

--- src/systems/camera/increment_camera.rs
+++ src/systems/camera/increment_camera.rs
@@ -2,23 +2,8 @@ use crate::resources::camera_settings::CameraSettings;
 use bevy::prelude::{Camera, Input, KeyCode, OrthographicProjection, Query, Res, ResMut, With};
 
 pub fn increment_camera(
-    input: Res<Input<KeyCode>>,
-    mut camera_projection_query: Query<&mut OrthographicProjection, With<Camera>>,
-    mut camera_settings: ResMut<CameraSettings>,
+    _input: Res<Input<KeyCode>>,
+    _camera_projection_query: Query<&mut OrthographicProjection, With<Camera>>,
+    _camera_settings: ResMut<CameraSettings>,
 ) {
-    // for (mut transform, mut player) in &mut characters {
-    // let player_speed = player.ship.current_velocity * time.delta_seconds();
-
-    // Increment Camera Zoom
-    // if input.pressed(KeyCode::Plus) {
-    //     player.ship.current_velocity = (player.ship.current_velocity
-    //         + player.ship.acceleration)
-    //         .clamp(-player.ship.velocity, player.ship.velocity);
-
-    //     let movement_direction = transform.rotation * Vec3::Y;
-    //     let translation_delta = movement_direction * player_speed;
-    //     transform.translation += translation_delta;
-    // }
-    // Backwards
-    // }
 }
