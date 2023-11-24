d0eed5aa7f61ba82e8a41816fe97633ad2857bf3
Skippy <steelstone3@gmail.com>
2023-11-24 02:40:45 +0000

    Update player_exotic_movement.rs

--- src/systems/weapons/player_exotic/player_exotic_movement.rs
+++ src/systems/weapons/player_exotic/player_exotic_movement.rs
@@ -11,3 +11,45 @@ pub fn player_exotic_movement(mut exotic: Query<(&mut Transform, &PlayerExotic)>
         exotic_transform.translation += translation_delta;
     }
 }
+
+#[cfg(test)]
+mod player_exotic_movement_should {
+    use bevy::{
+        prelude::{App, Update, Vec2},
+        time::{TimePlugin, Timer, TimerMode},
+    };
+
+    use crate::{
+        assets::{images::weapons::exotics::ExoticSprite, sounds::weapons::exotics::ExoticSound},
+        components::{exotic::Exotic, player_exotic::PlayerExotic},
+    };
+
+    use super::player_exotic_movement;
+
+    #[test]
+    fn move_exotic_projectile() {
+        // Given
+        let mut app = App::new();
+        app.add_systems(Update, player_exotic_movement);
+        let exotic_id = app
+            .world
+            .spawn(PlayerExotic {
+                exotic: Exotic {
+                    exotic: ExoticSprite::Exotic1,
+                    sound: ExoticSound::Exotic1,
+                    velocity: 75.0,
+                    size: Vec2::new(0.8, 0.8),
+                    lifetime: Timer::from_seconds(10.0, TimerMode::Once),
+                },
+            })
+            .id();
+        app.add_plugins(TimePlugin);
+
+        // When
+        app.update();
+
+        // Then
+        assert!(app.world.get::<PlayerExotic>(exotic_id).is_some());
+        // TODO test actual movement
+    }
+}
