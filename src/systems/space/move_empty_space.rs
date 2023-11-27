edd81c0e8bafcb15c323a6d44f02b5620beeab13
matthew <demengies@gmail.com>
2023-11-27 20:23:37 +0000

    optimizations

--- src/systems/space/move_empty_space.rs
+++ src/systems/space/move_empty_space.rs
@@ -32,23 +32,15 @@ pub fn move_empty_space(
         .ceil()
         .clamp(sector_size.bottom_border, sector_size.top_border) as i32;
 
-    let mut space_enumeration = space_query.iter_mut().enumerate();
-    let mut used_indexs: [i32; 9] = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
+    let mut space_enumeration = space_query.iter_mut();
     for x in x_start..x_start + number_of_tiles {
         for y in y_start..y_start + number_of_tiles {
-            for (i, mut space_item) in &mut space_enumeration {
-                let index_int = i as i32;
-                if !used_indexs.contains(&index_int) {
-                    space_item.0.translation = Vec3::new(
-                        (x as f32 * space_tile_size) - total_area_around_player
-                            + half_space_tile_size,
-                        (y as f32 * space_tile_size) - total_area_around_player
-                            + half_space_tile_size,
-                        0.0,
-                    );
-                    used_indexs[i] = index_int;
-                    break;
-                }
+            if let Some(mut space_item) = space_enumeration.next() {
+                space_item.0.translation = Vec3::new(
+                    (x as f32 * space_tile_size) - total_area_around_player + half_space_tile_size,
+                    (y as f32 * space_tile_size) - total_area_around_player + half_space_tile_size,
+                    0.0,
+                );
             }
         }
     }
