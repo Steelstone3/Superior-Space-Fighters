6c7bfe4f3670cce5d0dfeadf5e06e32910b0f172
Skippy <steelstone3@gmail.com>
2023-11-28 00:10:34 +0000

    Update space.rs

--- src/assets/images/space.rs
+++ src/assets/images/space.rs
@@ -42,101 +42,149 @@ impl Display for SpaceSprite {
     fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             SpaceSprite::Space1 => {
-                write!(formatter, "images/background/Starfields/Starfield_01.png")
-            },
+                write!(formatter, "images/background/space/space_1.png")
+            }
             SpaceSprite::Space2 => {
-                write!(formatter, "images/background/Starfields/Starfield_02.png")
-            },
+                write!(formatter, "images/background/space/space_2.png")
+            }
             SpaceSprite::Space3 => {
-                write!(formatter, "images/background/Starfields/Starfield_03.png")
-            },
+                write!(formatter, "images/background/space/space_3.png")
+            }
             SpaceSprite::Space4 => {
-                write!(formatter, "images/background/Starfields/Starfield_04.png")
-            },
+                write!(formatter, "images/background/space/space_4.png")
+            }
             SpaceSprite::Space5 => {
-                write!(formatter, "images/background/Starfields/Starfield_05.png")
-            },
+                write!(formatter, "images/background/space/space_5.png")
+            }
             SpaceSprite::Space6 => {
-                write!(formatter, "images/background/Starfields/Starfield_06.png")
+                write!(formatter, "images/background/space/space_6.png")
             }
             SpaceSprite::Space7 => {
-                write!(formatter, "images/background/Starfields/Starfield_07.png")
+                write!(formatter, "images/background/space/space_7.png")
             }
             SpaceSprite::Space8 => {
-                write!(formatter, "images/background/Starfields/Starfield_08.png")
+                write!(formatter, "images/background/space/space_8.png")
             }
             SpaceSprite::Space9 => {
-                write!(formatter, "images/background/Purple Nebula/Purple_Nebula_01.png")
-            },
+                write!(
+                    formatter,
+                    "images/background/purple_nebula/purple_nebula_1.png"
+                )
+            }
             SpaceSprite::Space10 => {
-                write!(formatter, "images/background/Purple Nebula/Purple_Nebula_02.png")
-            },
+                write!(
+                    formatter,
+                    "images/background/purple_nebula/purple_nebula_2.png"
+                )
+            }
             SpaceSprite::Space11 => {
-                write!(formatter, "images/background/Purple Nebula/Purple_Nebula_03.png")
-            },
+                write!(
+                    formatter,
+                    "images/background/purple_nebula/purple_nebula_3.png"
+                )
+            }
             SpaceSprite::Space12 => {
-                write!(formatter, "images/background/Purple Nebula/Purple_Nebula_04.png")
-            },
+                write!(
+                    formatter,
+                    "images/background/purple_nebula/purple_nebula_4.png"
+                )
+            }
             SpaceSprite::Space13 => {
-                write!(formatter, "images/background/Purple Nebula/Purple_Nebula_05.png")
-            },
+                write!(
+                    formatter,
+                    "images/background/purple_nebula/purple_nebula_5.png"
+                )
+            }
             SpaceSprite::Space14 => {
-                write!(formatter, "images/background/Purple Nebula/Purple_Nebula_06.png")
+                write!(
+                    formatter,
+                    "images/background/purple_nebula/purple_nebula_6.png"
+                )
             }
             SpaceSprite::Space15 => {
-                write!(formatter, "images/background/Purple Nebula/Purple_Nebula_07.png")
+                write!(
+                    formatter,
+                    "images/background/purple_nebula/purple_nebula_7.png"
+                )
             }
             SpaceSprite::Space16 => {
-                write!(formatter, "images/background/Purple Nebula/Purple_Nebula_08.png")
+                write!(
+                    formatter,
+                    "images/background/purple_nebula/purple_nebula_8.png"
+                )
             }
             SpaceSprite::Space17 => {
-                write!(formatter, "images/background/Green Nebula/Green_Nebula_08.png")
-            },
+                write!(
+                    formatter,
+                    "images/background/green_nebula/green_nebula_1.png"
+                )
+            }
             SpaceSprite::Space18 => {
-                write!(formatter, "images/background/Green Nebula/Green_Nebula_08.png")
-            },
+                write!(
+                    formatter,
+                    "images/background/green_nebula/green_nebula_2.png"
+                )
+            }
             SpaceSprite::Space19 => {
-                write!(formatter, "images/background/Green Nebula/Green_Nebula_08.png")
-            },
+                write!(
+                    formatter,
+                    "images/background/green_nebula/green_nebula_3.png"
+                )
+            }
             SpaceSprite::Space20 => {
-                write!(formatter, "images/background/Green Nebula/Green_Nebula_08.png")
-            },
+                write!(
+                    formatter,
+                    "images/background/green_nebula/green_nebula_4.png"
+                )
+            }
             SpaceSprite::Space21 => {
-                write!(formatter, "images/background/Green Nebula/Green_Nebula_08.png")
-            },
+                write!(
+                    formatter,
+                    "images/background/green_nebula/green_nebula_5.png"
+                )
+            }
             SpaceSprite::Space22 => {
-                write!(formatter, "images/background/Green Nebula/Green_Nebula_08.png")
-            },
+                write!(
+                    formatter,
+                    "images/background/green_nebula/green_nebula_6.png"
+                )
+            }
             SpaceSprite::Space23 => {
-                write!(formatter, "images/background/Green Nebula/Green_Nebula_08.png")
-            },
+                write!(
+                    formatter,
+                    "images/background/green_nebula/green_nebula_7.png"
+                )
+            }
             SpaceSprite::Space24 => {
-                write!(formatter, "images/background/Green Nebula/Green_Nebula_08.png")
-            },
+                write!(
+                    formatter,
+                    "images/background/green_nebula/green_nebula_8.png"
+                )
+            }
             SpaceSprite::Space25 => {
-                write!(formatter, "images/background/Blue Nebula/Blue_Nebula_01.png")
-            },
+                write!(formatter, "images/background/blue_nebula/blue_nebula_1.png")
+            }
             SpaceSprite::Space26 => {
-                write!(formatter, "images/background/Blue Nebula/Blue_Nebula_02.png")
-            },
+                write!(formatter, "images/background/blue_nebula/blue_nebula_2.png")
+            }
             SpaceSprite::Space27 => {
-                write!(formatter, "images/background/Blue Nebula/Blue_Nebula_03.png")
-            },
+                write!(formatter, "images/background/blue_nebula/blue_nebula_3.png")
+            }
             SpaceSprite::Space28 => {
-                write!(formatter, "images/background/Blue Nebula/Blue_Nebula_04.png")
-            },
+                write!(formatter, "images/background/blue_nebula/blue_nebula_4.png")
+            }
             SpaceSprite::Space29 => {
-                write!(formatter, "images/background/Blue Nebula/Blue_Nebula_05.png")
-            },
+                write!(formatter, "images/background/blue_nebula/blue_nebula_5.png")
+            }
             SpaceSprite::Space30 => {
-                write!(formatter, "images/background/Blue Nebula/Blue_Nebula_06.png")
-            },
+                write!(formatter, "images/background/blue_nebula/blue_nebula_6.png")
+            }
             SpaceSprite::Space31 => {
-                write!(formatter, "images/background/Blue Nebula/Blue_Nebula_07.png")
-            },
+                write!(formatter, "images/background/blue_nebula/blue_nebula_7.png")
+            }
             SpaceSprite::Space32 => {
-                write!(formatter, "images/background/Blue Nebula/Blue_Nebula_08.png")
-            },
+                write!(formatter, "images/background/blue_nebula/blue_nebula_8.png")
+            }
         }
     }
 }
