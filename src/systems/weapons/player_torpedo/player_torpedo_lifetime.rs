333a2c5a110eddade1af1840313860c366a553b3
Skippy <steelstone3@gmail.com>
2023-12-03 23:36:15 +0000

    Update player_torpedo_lifetime.rs

--- src/systems/weapons/player_torpedo/player_torpedo_lifetime.rs
+++ src/systems/weapons/player_torpedo/player_torpedo_lifetime.rs
@@ -1,18 +1,18 @@
+use crate::{
+    components::player_torpedo::PlayerTorpedo,
+    resources::projectile_ammunition::ProjectileAmmunition,
+};
 use bevy::{
     prelude::{Commands, Entity, Query, Res, ResMut},
     time::Time,
     utils::tracing,
 };
 
-use crate::{
-    components::player_torpedo::PlayerTorpedo, resources::torpedo_ammunition::TorpedoAmmunition,
-};
-
 pub fn player_torpedo_lifetime(
     mut commands: Commands,
     time: Res<Time>,
     mut torpedo: Query<(Entity, &mut PlayerTorpedo)>,
-    mut torpedo_ammunition: ResMut<TorpedoAmmunition>,
+    mut ammunition: ResMut<ProjectileAmmunition>,
 ) {
     for (torpedo_entity, mut torpedo) in &mut torpedo {
         torpedo.torpedo.lifetime.tick(time.delta());
@@ -20,11 +20,11 @@ pub fn player_torpedo_lifetime(
         if torpedo.torpedo.lifetime.finished() {
             commands.entity(torpedo_entity).despawn();
 
-            torpedo_ammunition.0 += 1;
+            ammunition.torpedo_ammunition += 1;
 
             tracing::info!(
                 "Torpedo ammunition recovered. Current torpedo ammunition: {:?}",
-                torpedo_ammunition.0
+                ammunition.torpedo_ammunition
             );
         }
     }
