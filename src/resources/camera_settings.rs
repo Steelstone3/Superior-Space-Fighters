4eace1d79f120fa2c1525ad81c9de5f4a7f37d34
Skippy <70329398+Steelstone3@users.noreply.github.com>
2023-12-04 00:50:28 +0000

    Camera adjustment

--- src/resources/camera_settings.rs
+++ src/resources/camera_settings.rs
@@ -3,7 +3,9 @@ use bevy::ecs::system::Resource;
 #[derive(Resource)]
 pub struct CameraSettings {
     pub zoom_speed: f32,
-    pub min_zoom: f32,
-    pub max_zoom: f32,
+    pub minimum_zoom: f32,
+    pub maximum_zoom: f32,
     pub current_zoom: f32,
+    pub zoom_in: f32,
+    pub zoom_out: f32,
 }
--- src/systems/camera/scroll_camera.rs
+++ src/systems/camera/scroll_camera.rs
@@ -1,3 +1,4 @@
+use crate::resources::camera_settings::CameraSettings;
 use bevy::{
     ecs::{
         event::EventReader,
@@ -9,8 +10,6 @@ use bevy::{
 };
 use float_lerp::lerp;
 
-use crate::resources::camera_settings::CameraSettings;
-
 pub fn scroll_camera(
     mut scroll_event_reader: EventReader<MouseWheel>,
     mut camera_projection_query: Query<&mut OrthographicProjection, With<Camera>>,
@@ -20,28 +19,32 @@ pub fn scroll_camera(
 
     for event in scroll_event_reader.read() {
         match event.unit {
-            //For mouse
+            // For mouse
             MouseScrollUnit::Line => {
                 if event.y < 0.0 {
-                    camera_settings.current_zoom =
-                        (camera_settings.current_zoom * 1.25 * camera_settings.zoom_speed)
-                            .clamp(camera_settings.min_zoom, camera_settings.max_zoom);
+                    camera_settings.current_zoom = (camera_settings.current_zoom
+                        * camera_settings.zoom_in
+                        * camera_settings.zoom_speed)
+                        .clamp(camera_settings.minimum_zoom, camera_settings.maximum_zoom);
                 } else if event.y > 0.0 {
-                    camera_settings.current_zoom = (camera_settings.current_zoom * 0.75
+                    camera_settings.current_zoom = (camera_settings.current_zoom
+                        * camera_settings.zoom_out
                         / camera_settings.zoom_speed)
-                        .clamp(camera_settings.min_zoom, camera_settings.max_zoom);
+                        .clamp(camera_settings.minimum_zoom, camera_settings.maximum_zoom);
                 }
             }
-            //For touchpads
+            // For touchpads
             MouseScrollUnit::Pixel => {
                 if event.y < 0.0 {
-                    camera_settings.current_zoom =
-                        (camera_settings.current_zoom * 1.25 * camera_settings.zoom_speed)
-                            .clamp(camera_settings.min_zoom, camera_settings.max_zoom);
+                    camera_settings.current_zoom = (camera_settings.current_zoom
+                        * camera_settings.zoom_in
+                        * camera_settings.zoom_speed)
+                        .clamp(camera_settings.minimum_zoom, camera_settings.maximum_zoom);
                 } else if event.y > 0.0 {
-                    camera_settings.current_zoom = (camera_settings.current_zoom * 0.75
+                    camera_settings.current_zoom = (camera_settings.current_zoom
+                        * camera_settings.zoom_out
                         / camera_settings.zoom_speed)
-                        .clamp(camera_settings.min_zoom, camera_settings.max_zoom);
+                        .clamp(camera_settings.minimum_zoom, camera_settings.maximum_zoom);
                 }
             }
         }
