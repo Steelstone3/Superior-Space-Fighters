1342b1074d421a34217243b2493e17f365ebe61f
Skippy <steelstone3@gmail.com>
2024-02-22 17:10:26 +0000

    update

--- src/components/station.rs
+++ src/components/station.rs
@@ -1,16 +1,37 @@
+use crate::assets::images::space_stations::SpaceStationSprite;
 use bevy::{ecs::component::Component, prelude::Vec2};
 
-use crate::assets::images::space_stations::SpaceStationSprite;
-
-#[derive(Component)]
+#[derive(Component, Debug, PartialEq)]
 pub struct Station {
     pub station: SpaceStationSprite,
     pub size: Vec2,
 }
 
+impl Station {
+    pub fn new(station: SpaceStationSprite, size: f32) -> Self {
+        Self {
+            station,
+            size: Vec2::new(size, size),
+        }
+    }
+}
+
 #[cfg(test)]
 mod station_should {
+    use super::*;
+
     #[test]
-    #[ignore]
-    fn create_new() {}
+    fn create_new() {
+        // Given
+        let expected_station = Station {
+            station: SpaceStationSprite::AstralSovereignStation1,
+            size: Vec2::new(100.0, 100.0),
+        };
+
+        // When
+        let station = Station::new(SpaceStationSprite::AstralSovereignStation1, 100.0);
+
+        // Then
+        assert_eq!(expected_station, station)
+    }
 }
