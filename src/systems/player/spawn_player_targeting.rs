102fed64ba4721f2a02240e908d8a77be1b8597d
matthew <demengies@gmail.com>
2024-03-01 20:43:06 +0000

    comments

--- src/systems/player/spawn_player_targeting.rs
+++ src/systems/player/spawn_player_targeting.rs
@@ -2,6 +2,7 @@ use bevy::{
     ecs::{
         entity::Entity,
         query::{With, Without},
+        query::{With, Without},
         system::{Commands, Query, Res},
     },
     input::{keyboard::KeyCode, ButtonInput},
@@ -17,10 +18,13 @@ use crate::components::{
 };
 
 pub fn spawn_player_targeting<'a>(
+pub fn spawn_player_targeting<'a>(
     input: Res<ButtonInput<KeyCode>>,
     mut commands: Commands,
     other_ships: Query<(&Transform, &Starship, Entity), Without<PlayerStarship>>,
+    other_ships: Query<(&Transform, &Starship, Entity), Without<PlayerStarship>>,
     mut existing_target: Query<&mut Target>,
+    player_location: Query<&Transform, (With<PlayerStarship>, Without<Starship>)>,
     player_location: Query<&Transform, (With<PlayerStarship>, Without<Starship>)>,
 ) {
     if input.just_pressed(KeyCode::KeyT) {
@@ -31,6 +35,7 @@ pub fn spawn_player_targeting<'a>(
         let mut closest_ship: Option<(&Transform, &Starship, Entity)> = None;
         let mut distance = 999.0;
         let mut ship_count = 0;
+        //get closest target ship
         for other_ship in other_ships.into_iter() {
             ship_count += 1;
             let new_distance = (other_ship.0.translation - player_location.translation).length();
@@ -47,11 +52,19 @@ pub fn spawn_player_targeting<'a>(
 
         match closest_ship {
             Some(closest_target) => {
+                //if target already exists update target else create new target
                 let Ok(mut existing_target) = existing_target.get_single_mut() else {
                     let target = Target {
                         target_entity: closest_target.2,
                     };
 
+                    let sprite = Sprite {
+                        color: Color::rgba(1.0, 1.0, 1.0, 0.1),
+                        flip_x: false,
+                        flip_y: false,
+                        custom_size: Some(Vec2::new(100.0, 100.0)),
+                        ..Default::default()
+                    };
                     let sprite = Sprite {
                         color: Color::rgba(1.0, 1.0, 1.0, 0.1),
                         flip_x: false,
@@ -67,12 +80,25 @@ pub fn spawn_player_targeting<'a>(
                             ..Default::default()
                         })
                         .insert(target);
+                    commands
+                        .spawn(SpriteBundle {
+                            sprite,
+                            transform: *closest_target.0,
+                            ..Default::default()
+                        })
+                        .insert(target);
 
+                    return;
+                };
                     return;
                 };
 
                 existing_target.target_entity = closest_target.2;
+                existing_target.target_entity = closest_target.2;
 
+                log::info!("Target Locked");
+            }
+            None => {}
                 log::info!("Target Locked");
             }
             None => {}
