e94dbd8fac5b5b3b8a341eb62711495cd18c6d18
matthew <demengies@gmail.com>
2024-03-01 20:43:06 +0000

    Target closest ship

--- src/systems/player/spawn_player_targeting.rs
+++ src/systems/player/spawn_player_targeting.rs
@@ -1,7 +1,7 @@
 use bevy::{
     ecs::{
         entity::Entity,
-        query::Without,
+        query::{With, Without},
         system::{Commands, Query, Res},
     },
     input::{keyboard::KeyCode, ButtonInput},
@@ -16,42 +16,66 @@ use crate::components::{
     player_starship::PlayerStarship, starship::Starship, weapons::target::Target,
 };
 
-pub fn spawn_player_targeting(
+pub fn spawn_player_targeting<'a>(
     input: Res<ButtonInput<KeyCode>>,
     mut commands: Commands,
-    mut other_ships: Query<(&Transform, &Starship, Entity), Without<PlayerStarship>>,
+    other_ships: Query<(&Transform, &Starship, Entity), Without<PlayerStarship>>,
     mut existing_target: Query<&mut Target>,
+    player_location: Query<&Transform, (With<PlayerStarship>, Without<Starship>)>,
 ) {
     if input.just_pressed(KeyCode::KeyT) {
-        // for other_ship in &mut other_ships {
-        if let Some(other_ship) = (&mut other_ships).into_iter().next() {
-            let Ok(mut target) = existing_target.get_single_mut() else {
-                let target = Target {
-                    target_entity: other_ship.2,
-                };
+        let Ok(player_location) = player_location.get_single() else {
+            return;
+        };
 
-                let sprite = Sprite {
-                    color: Color::rgba(1.0, 1.0, 1.0, 0.1),
-                    flip_x: false,
-                    flip_y: false,
-                    custom_size: Some(Vec2::new(100.0, 100.0)),
-                    ..Default::default()
-                };
+        let mut closest_ship: Option<(&Transform, &Starship, Entity)> = None;
+        let mut distance = 999.0;
+        let mut ship_count = 0;
+        for other_ship in other_ships.into_iter() {
+            ship_count += 1;
+            let new_distance = (other_ship.0.translation - player_location.translation).length();
+            if new_distance < distance {
+                distance = new_distance;
+                closest_ship = Some(other_ship);
+            } else {
+                continue;
+            }
+        }
 
-                commands
-                    .spawn(SpriteBundle {
-                        sprite,
-                        transform: *other_ship.0,
+        log::info!("{ship_count} ships to target");
+        log::info!("Closest ship is {distance} units away");
+
+        match closest_ship {
+            Some(closest_target) => {
+                let Ok(mut existing_target) = existing_target.get_single_mut() else {
+                    let target = Target {
+                        target_entity: closest_target.2,
+                    };
+
+                    let sprite = Sprite {
+                        color: Color::rgba(1.0, 1.0, 1.0, 0.1),
+                        flip_x: false,
+                        flip_y: false,
+                        custom_size: Some(Vec2::new(100.0, 100.0)),
                         ..Default::default()
-                    })
-                    .insert(target);
+                    };
 
-                return;
-            };
+                    commands
+                        .spawn(SpriteBundle {
+                            sprite,
+                            transform: *closest_target.0,
+                            ..Default::default()
+                        })
+                        .insert(target);
 
-            target.target_entity = other_ship.2;
+                    return;
+                };
 
-            log::info!("Target Locked");
+                existing_target.target_entity = closest_target.2;
+
+                log::info!("Target Locked");
+            }
+            None => {}
         }
     }
 }
