41fe1fbc4a18e37e5c1ff424160ea166f690802e
Skippy <steelstone3@gmail.com>
2024-03-15 23:11:24 +0000

    Update despawn_target.rs

--- src/systems/weapons/targetting/despawn_target.rs
+++ src/systems/weapons/targetting/despawn_target.rs
@@ -1,1 +1,49 @@
-// TODO on no starships remains or key press
+use crate::components::{
+    starships::starship::Starship,
+    weapons::weapon_types::{target::Target, targetting_setting::TargettingSettings},
+};
+use bevy::{
+    ecs::{
+        entity::Entity,
+        system::{Commands, Query, Res},
+    },
+    input::{keyboard::KeyCode, ButtonInput},
+};
+
+#[allow(dead_code)]
+pub fn despawn_target(
+    mut commands: Commands,
+    input: Res<ButtonInput<KeyCode>>,
+    mut targetting_settings: Query<&mut TargettingSettings>,
+    targets: Query<(Entity, &Target)>,
+    starships: Query<&Starship>,
+) {
+    let Ok(mut targetting_setting) = targetting_settings.get_single_mut() else {
+        return;
+    };
+
+    let Ok(target) = targets.get_single() else {
+        return;
+    };
+
+    if !input.just_pressed(KeyCode::Tab) {
+        commands.entity(target.0).despawn();
+
+        targetting_setting.is_targetting = false;
+        return;
+    }
+
+    let mut random_starship = None;
+
+    for starship in starships.into_iter() {
+        random_starship = Some(starship)
+    }
+
+    match random_starship {
+        Some(_) => {}
+        None => {
+            commands.entity(target.0).despawn();
+            targetting_setting.is_targetting = false;
+        }
+    }
+}
