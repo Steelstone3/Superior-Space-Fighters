cf4e861e806f3324aa90cf30b3cf7a1d56ee1fa2
Skippy <steelstone3@gmail.com>
2024-03-28 18:44:15 +0000

    Update player_torpedo_collision.rs

--- src/systems/weapons/player_weapons/player_torpedo/player_torpedo_collision.rs
+++ src/systems/weapons/player_weapons/player_torpedo/player_torpedo_collision.rs
@@ -1,35 +1,41 @@
 use bevy::{
     asset::AssetServer,
     audio::{AudioBundle, PlaybackMode, PlaybackSettings, Volume},
-    ecs::{query::Without, system::Res},
-    prelude::{Commands, Entity, Query},
-    transform::components::Transform,
+    ecs::system::Res,
+    prelude::{Commands, Query},
     utils::tracing,
 };
 
-use crate::components::{
-    starships::starship::Starship, weapons::player_weapons::player_torpedo::PlayerTorpedo,
+use crate::queries::{
+    player_torpedo_queries::{MutablePlayerTorpedoEntityTransformQuery, PlayerTorpedoFilter},
+    starship_queries::{MutableStarshipEntityTransformQuery, StarshipFilter},
 };
 
 // TODO multi-thread
 pub fn player_torpedo_collision_with_starship(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut torpedoes: Query<(Entity, &mut Transform, &mut PlayerTorpedo), Without<Starship>>,
-    mut starships: Query<(Entity, &mut Transform, &mut Starship), Without<PlayerTorpedo>>,
+    mut player_torpedoes: Query<MutablePlayerTorpedoEntityTransformQuery, PlayerTorpedoFilter>,
+    mut starships: Query<MutableStarshipEntityTransformQuery, StarshipFilter>,
 ) {
-    for (torpedo_entity, torpedo_transform, mut torpedo) in &mut torpedoes {
-        for (starship_entity, starship_transform, mut starship) in &mut starships {
+    for mut player_torpedo in &mut player_torpedoes {
+        for mut starship in &mut starships {
             let distance_to_starship =
-                (torpedo_transform.translation - starship_transform.translation).length();
+                (player_torpedo.transform.translation - starship.transform.translation).length();
 
-            let is_collision =
-                distance_to_starship < starship.size.x || distance_to_starship < starship.size.y;
+            let is_collision = distance_to_starship < starship.starship.size.x
+                || distance_to_starship < starship.starship.size.y;
 
             if is_collision {
                 tracing::info!("Torpedo collision with starship");
                 commands.spawn(AudioBundle {
-                    source: asset_server.load(torpedo.torpedo.impact_sound.to_string()),
+                    source: asset_server.load(
+                        player_torpedo
+                            .player_torpedo
+                            .torpedo
+                            .impact_sound
+                            .to_string(),
+                    ),
                     settings: PlaybackSettings {
                         mode: PlaybackMode::Once,
                         volume: Volume::new(0.2),
@@ -37,25 +43,34 @@ pub fn player_torpedo_collision_with_starship(
                     },
                 });
 
-                torpedo
+                player_torpedo
+                    .player_torpedo
                     .torpedo
                     .lock_on_weapon
                     .ranged_weapon
                     .weapon
                     .damage
                     .calculate_damage();
-                starship.take_damage(torpedo.torpedo.lock_on_weapon.ranged_weapon.weapon.damage);
+                starship.starship.take_damage(
+                    player_torpedo
+                        .player_torpedo
+                        .torpedo
+                        .lock_on_weapon
+                        .ranged_weapon
+                        .weapon
+                        .damage,
+                );
 
                 tracing::info!(
                     "Enemy Starship | Shield: {:?} | Health: {:?} |",
-                    starship.shield.current,
-                    starship.hull.current,
+                    starship.starship.shield.current,
+                    starship.starship.hull.current,
                 );
 
-                commands.entity(torpedo_entity).despawn();
+                commands.entity(player_torpedo.entity).despawn();
 
-                if starship.is_destroyed() {
-                    commands.entity(starship_entity).despawn();
+                if starship.starship.is_destroyed() {
+                    commands.entity(starship.entity).despawn();
                     tracing::info!("Enemy Starship Destroyed");
                 }
             }
