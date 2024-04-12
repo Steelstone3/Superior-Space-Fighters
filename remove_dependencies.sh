835a037eaeabbd6ae84e0fe0003fc318f5b3c816
MatthewGriffin <demengies@gmail.com>
2024-04-12 19:05:39 +0100

    add lld clang to setup/remove_dependencies

--- remove_dependencies.sh
+++ remove_dependencies.sh
@@ -1,7 +1,9 @@
 # Ubuntu
 sudo apt remove libx11-dev libasound2-dev libudev-dev libxkbcommon-x11-0 librust-alsa-sys-dev librust-libudev-sys-dev libwayland-dev libxkbcommon-dev
 doas apt remove libx11-dev libasound2-dev libudev-dev libxkbcommon-x11-0 librust-alsa-sys-dev librust-libudev-sys-dev libwayland-dev libxkbcommon-dev
+sudo apt remove lld clang
 
 # Fedora
 sudo dnf5 remove libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
 doas dnf5 remove libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
+sudo dnf remove lld clang
--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -3,9 +3,11 @@ sudo apt install --fix-missing g++ pkg-config
 doas apt install --fix-missing g++ pkg-config
 sudo apt install --fix-missing libx11-dev libasound2-dev libudev-dev libxkbcommon-x11-0 librust-alsa-sys-dev librust-libudev-sys-dev libwayland-dev libxkbcommon-dev
 doas apt install --fix-missing libx11-dev libasound2-dev libudev-dev libxkbcommon-x11-0 librust-alsa-sys-dev librust-libudev-sys-dev libwayland-dev libxkbcommon-dev
+sudo apt-get install lld clang
 
 # Fedora
 sudo dnf install gcc-c++ 
 doas dnf install gcc-c++ 
 sudo dnf5 install libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
 doas dnf5 install libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
+sudo dnf install lld clang
