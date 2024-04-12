350b06ecb51ec48119162b375d88b6f02087a3f6
Skippy <steelstone3@gmail.com>
2024-04-12 19:46:04 +0100

    update

--- remove_dependencies.sh
+++ remove_dependencies.sh
@@ -1,19 +1,19 @@
 # Ubuntu
-sudo apt remove mold clang
-doas apt remove mold clang
+sudo apt remove lld clang
+doas apt remove lld clang
 sudo apt remove libx11-dev libasound2-dev libudev-dev libxkbcommon-x11-0 librust-alsa-sys-dev librust-libudev-sys-dev libwayland-dev libxkbcommon-dev
 doas apt remove libx11-dev libasound2-dev libudev-dev libxkbcommon-x11-0 librust-alsa-sys-dev librust-libudev-sys-dev libwayland-dev libxkbcommon-dev
 
 # Fedora
 
 ## dnf5
-sudo dnf5 remove mold clang
-doas dnf5 remove mold clang
+sudo dnf5 remove lld clang
+doas dnf5 remove lld clang
 sudo dnf5 remove libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
 doas dnf5 remove libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
 
 ## dnf4
-sudo dnf remove mold clang
-doas dnf remove mold clang
+sudo dnf remove lld clang
+doas dnf remove lld clang
 sudo dnf remove libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
 doas dnf remove libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -1,19 +1,19 @@
 # Ubuntu
-sudo apt install --fix-missing g++ pkg-config mold clang
-doas apt install --fix-missing g++ pkg-config mold clang
+sudo apt install --fix-missing g++ pkg-config lld clang
+doas apt install --fix-missing g++ pkg-config lld clang
 sudo apt install --fix-missing libx11-dev libasound2-dev libudev-dev libxkbcommon-x11-0 librust-alsa-sys-dev librust-libudev-sys-dev libwayland-dev libxkbcommon-dev
 doas apt install --fix-missing libx11-dev libasound2-dev libudev-dev libxkbcommon-x11-0 librust-alsa-sys-dev librust-libudev-sys-dev libwayland-dev libxkbcommon-dev
 
 # Fedora
 
 ## dnf5
-sudo dnf5 install gcc-c++ mold clang
-doas dnf5 install gcc-c++ mold clang
+sudo dnf5 install gcc-c++ lld clang
+doas dnf5 install gcc-c++ lld clang
 sudo dnf5 install libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
 doas dnf5 install libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
 
 ## dnf4
-sudo dnf install gcc-c++ mold clang
-doas dnf install gcc-c++ mold clang
+sudo dnf install gcc-c++ lld clang
+doas dnf install gcc-c++ lld clang
 sudo dnf install libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
 doas dnf install libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
