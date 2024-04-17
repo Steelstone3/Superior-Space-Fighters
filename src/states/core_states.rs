005ab7c42a8061b50784fa2244f33e2a242ef76b
MatthewGriffin <demengies@gmail.com>
2024-04-17 19:14:47 +0100

    remove loadingscreen state as not used

--- src/states/core_states.rs
+++ src/states/core_states.rs
@@ -3,7 +3,6 @@ use bevy::ecs::schedule::States;
 #[derive(States, Default, Debug, Clone, PartialEq, Eq, Hash)]
 pub enum GameState {
     Paused,
-    LoadingScreen,
     #[default]
     MainMenu,
     InGame,
