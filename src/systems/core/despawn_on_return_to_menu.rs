aecdd8f21a358aee78d755cefcc41555b05544a8
MatthewGriffin <demengies@gmail.com>
2024-04-22 13:41:24 +0100

    remove projectiles on quit to menu

--- src/systems/core/despawn_on_return_to_menu.rs
+++ src/systems/core/despawn_on_return_to_menu.rs
@@ -8,12 +8,32 @@ use bevy::{
 };
 
 use crate::{
-    components::{space::Space, starships::starship::Starship, station::SpaceStation},
+    components::{
+        space::Space,
+        starships::starship::Starship,
+        station::SpaceStation,
+        weapons::player_weapons::{
+            player_blaster::PlayerBlaster, player_exotic::PlayerExotic, player_mine::PlayerMine,
+            player_torpedo::PlayerTorpedo,
+        },
+    },
     queries::entity_query::EntityQuery,
 };
 
 pub fn despawn_on_return_to_menu(
-    entities: Query<EntityQuery, Or<(With<Space>, With<SpaceStation>, With<Starship>, With<Node>)>>,
+    entities: Query<
+        EntityQuery,
+        Or<(
+            With<Space>,
+            With<SpaceStation>,
+            With<Starship>,
+            With<Node>,
+            With<PlayerBlaster>,
+            With<PlayerMine>,
+            With<PlayerTorpedo>,
+            With<PlayerExotic>,
+        )>,
+    >,
     mut commands: Commands,
 ) {
     for entity in entities.iter() {
